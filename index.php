324f6397918812200edccc7888ff62613be3602b
x202041 <104370946+x202041@users.noreply.github.com>
2022-05-04 19:55:22 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>test</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+<!-- Start your code here -->
+<!-- X軸よ永遠に -->
+ <?php
+  echo $_POST['name']
+  ?>
+</body>
+</html>
